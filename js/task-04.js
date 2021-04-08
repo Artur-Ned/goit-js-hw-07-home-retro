a963d8fa414d2128bb9ffaa6f1f7e91c024a9002
NNASTYA2007 <advokat1707@gmail.com>
2021-04-08 20:50:46 +0300

    сделал 4дз

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,6 @@
 
-const counter = {
-    counterValue: 0,
+const counterValue = {
+   value: 0,
     increment() {
         console.log('increment -> this', this);
         this.value += 1;
@@ -11,33 +11,28 @@ const counter = {
     },
 };
 
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counterValueEl = document.querySelector('#value');
+
+
+decrementBtn.addEventListener('click', function () {
+    counterValue.decrement();
+    // console.log(counter);
+    counterValueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener('click', function () {
+    counterValue.increment();
+    // console.log(counter);
+    counterValueEl.textContent = counterValue.value;
+});
+
+
+
 // Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение
 // счетчика на 1.
 
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
-
-
-
-const decrementBtn = document.querySelector('.decrement');
-const incrementBtn = document.querySelector('.increment');
-const counterValueEl = document.querySelector('#value');
-
-decrementBtn.addEventListener('click', function () {
-    console.log('Кликнули на декремент');
-
-    counter.decrement();
-    console.log(counter);
-    counterValueEl.textContent = counter.value;
-});
-
-incrementBtn.addEventListener('click', function () {
-    console.log('Кликнули на инкремент');
-
-    counter.increment();
-    console.log(counter);
-    counterValueEl.textContent = counter.value;
-});
-
-console.log(window);
