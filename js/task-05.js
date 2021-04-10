32110595765114f7f73ca688e31902929beef843
NNASTYA2007 <advokat1707@gmail.com>
2021-04-10 21:43:48 +0300

    did task#5

---
+++ js/task-05.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#name-input');
+console.log(input);
+const span = document.querySelector('#name-output')
+// console.log(span);
+
+
+input.addEventListener('input', contentInputMassages);
+
+function contentInputMassages(masseges) {
+   
+    if (masseges.currentTarget.value === '')
+        {return span.textContent = 'незнакомец'}
+    return span.textContent = masseges.currentTarget.value;
+ 
+    
+}
+
+console.log(span);
+
+
+// Напиши скрипт который, при наборе текста в инпуте
+// input#name - input(событие input), подставляет его текущее
+// значение в span#name - output.Если инпут пустой, в спане должна
+// отображаться строка 'незнакомец'.
+
